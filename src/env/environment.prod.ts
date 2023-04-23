08b5a5de465fbe016d89e5593f369a8c343db223
David C <david.contreras@gorillalogic.com>
2023-04-22 23:43:37 -0600

    Delete unused files

--- src/env/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "./environment-definition";
-
-export const environment : Environment = {
-  production: true,
-  baseApi: "https://myfinancewebapicore.azurewebsites.net"
-};
