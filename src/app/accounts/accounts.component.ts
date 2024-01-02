aa10c382ad29a73634894a1baed1760aa68dd0b1
DavidGPC <dcontre10@gmail.com>
2024-01-02 00:05:32 -0600

    Revert "basic layout for drag grid"
    This reverts commit f1bf2454c45244abe48c99de828ac8318db4963d.

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
-  items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
   constructor() { }
 
   ngOnInit(): void {
